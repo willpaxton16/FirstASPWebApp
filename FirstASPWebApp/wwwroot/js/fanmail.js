1d7bb6cd1366d70538aea7a085736244cc763c51
willpaxton16 <90337820+willpaxton16@users.noreply.github.com>
2022-01-10 15:21:32 -0700

    Add project files.

---
+++ FirstASPWebApp/wwwroot/js/fanmail.js
@@ -0,0 +1,14 @@
+﻿//document.getElementById("buttonsend").addEventListener("click", function () {
+//    alert("From: " + document.getElementById("txtfrom").value +
+//        ", Subject: " + document.getElementById("txtsubject").value + 
+//        ", Message: " + document.getElementById("txtmessage").value);
+//})
+
+
+$("#buttonsend").click(function () {
+    $("#gandimg").fadeToggle();
+    alert("From: " + $("#txtfrom").val() +
+        ", Subject: " + $("#txtsubject").val() +
+        ", Message: " + $("#txtmessage").val());
+})
+
